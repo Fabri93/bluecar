a6f4e460321cdf4ef4e3e78b09d3d8dd6630e985
Fabrizio Lasaponara <fabrizio.lasaponara@hotmail.it>
2018-03-24 19:19:43 +0100

    First Commit

---
+++ js/functions.js
@@ -0,0 +1,43 @@
+(function ($) {
+	
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.navbar-nav li a').bind('click', function(event) {
+			var $anchor = $(this);
+			var nav = $($anchor.attr('href'));
+			if (nav.length) {
+			$('html, body').stop().animate({				
+				scrollTop: $($anchor.attr('href')).offset().top				
+			}, 1500, 'easeInOutExpo');
+			
+			event.preventDefault();
+			}
+		});
+						
+	});	
+	
+	$('body').flipLightBox({
+			
+		lightbox_text_status: 0,
+		lightbox_navigation_status: 0
+    
+	})
+	
+	$('.parallax-window').parallax({imageSrc: 'img/3.png'});
+	
+		
+
+})(jQuery);
+
+ (     function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      })
+    
