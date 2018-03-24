883814f0127dabd5a59a8aef29439e9ce5090965
Fabrizio Lasaponara <fabrizio.lasaponara@hotmail.it>
2018-03-24 19:26:50 +0100

    ss

--- js/functions.js
+++ js/functions.js
@@ -29,6 +29,38 @@
 
 })(jQuery);
 
+
+(function ($) {
+	
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.form-inline div button').bind('click', function(event) {
+			var $anchor = $(this);
+			var nav = $($anchor.attr('href'));
+			if (nav.length) {
+			$('html, body').stop().animate({				
+				scrollTop: $($anchor.attr('href')).offset().top				
+			}, 1500, 'easeInOutExpo');
+			
+			event.preventDefault();
+			}
+		});
+						
+	});	
+	
+	$('body').flipLightBox({
+			
+		lightbox_text_status: 0,
+		lightbox_navigation_status: 0
+    
+	})
+	
+	$('.parallax-window').parallax({imageSrc: 'img/3.png'});
+	
+		
+
+})(jQuery);
+
  (     function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
